0b733f1b10d5ff2f006120e725cc802ec16b64f1
Jaime <jaime.caminero@live.u-tad.com>
2025-12-03 22:27:35 +0100

    Comentarios

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -1,10 +1,9 @@
 package App;
 
-import java.io.IOException;
-import java.net.ConnectException;
 import java.util.Scanner;
 
 public class Main {
+	// Creamos un método menú que consistirá en un bucle con switch
 	public void Menu() {
 		Scanner sc = new Scanner(System.in);
 		Finnhub miFinn = new Finnhub();
@@ -19,7 +18,7 @@ public class Main {
 			System.out.println("5. Salir");
 			System.out.print("Opción escogida: ");
 			// Necesitaremos validar si el usuario pulsa enter o si mete una letra que luego
-			// da error en el parseo
+			// da error en el parseo, por eso lo tratamos como String
 			int respuesta = -1;
 			boolean respuestaValida = false;
 			while (!respuestaValida) {
@@ -34,22 +33,30 @@ public class Main {
 					if (respuesta >= 1 && respuesta <= 5) {
 						respuestaValida = true;
 					} else {
+						// Si el número está fuera de rango lo indicamos
 						System.out.print("Introduce un número entre el 1 y 5: ");
 					}
+					// Si al parsear es una letra o caracter especial lo indicaremos
 				} catch (NumberFormatException e) {
 					System.out.print("Debes introducir un número válido: ");
 				}
 			}
+			// Creamos el switch
 			switch (respuesta) {
+			// Este caso pide el nombre o ticker de la empresa para poder mostrar datos
+			// principales
 			case 1:
 				System.out.print("\nIntroduce el nombre o Ticker de la empresa que quieres buscar: ");
 				String nombreEmpresa = sc.nextLine();
+				// Si se hace enter indicaremos y entraremos en bucle hasta que se corrija
 				while (nombreEmpresa.trim().isEmpty()) {
 					System.out.print("No puedes dejarlo vacío. Introduce un nombre válido: ");
 					nombreEmpresa = sc.nextLine();
 				}
+				// Llamamos al método de la clase Finnhub con el parámetro de entrada
 				miFinn.mostrarSymbolLookup(nombreEmpresa);
 				break;
+			// Caso para ver noticias de un ticker
 			case 2:
 				System.out.print("Introduce el Ticker de la empresa de la que quieres ver noticias: ");
 				String tickerEmpresa = sc.nextLine();
@@ -59,12 +66,13 @@ public class Main {
 				}
 				miFinn.obtenerNoticias(tickerEmpresa);
 				break;
+			// Caso para ver SEC Fillings de una empresa
 			case 3:
 				System.out.print("Introduce el Ticker de la empresa de la que quieres ver los SEC Fillings: ");
 				String tickerFillings = sc.nextLine();
 				while (tickerFillings.trim().isEmpty()) {
 					System.out.print("No puedes dejarlo vacío. Introduce un ticker válido: ");
-					tickerEmpresa = sc.nextLine();
+					tickerFillings = sc.nextLine();
 				}
 				tickerFillings.trim();
 				if (miFinn.mostrarSECFilling(tickerFillings)) {
@@ -104,13 +112,19 @@ public class Main {
 				break;
 
 			case 4:
+				// Para guardar un ticker llamaremos al método correspondiente
 				System.out.print("Introduce el ticker de la empresa que quieres guardar:");
 				String tickerFichero = sc.nextLine();
+				while (tickerFichero.trim().isEmpty()) {
+					System.out.print("No puedes dejarlo vacío. Introduce un ticker válido: ");
+					tickerFichero = sc.nextLine();
+				}
 				miFinn.guardarWatchlist(tickerFichero);
 				break;
+			// Método para cerra el programa
 			case 5:
 				continuar = false;
-				System.out.println("Saliendo....");
+				System.out.println("Gracias por usar nuestro programa. Saliendo....");
 				break;
 
 			default:
@@ -122,12 +136,13 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		Finnhub main = new Finnhub();
+		// Cramos objeto finnhub para llamar a los métodos
+		Finnhub metodos = new Finnhub();
 		Main miMain = new Main();
 		Scanner sc = new Scanner(System.in);
 		try {
-			main.mostrarInfoBase("QQQ");
-			main.mostrarInfoBase("SPY");
+			metodos.mostrarInfoBase("QQQ");
+			metodos.mostrarInfoBase("SPY");
 			miMain.Menu();
 		} catch (ErrorConnectException e) {
 			System.out.println("Ha ocurrido un error de conexión: " + e.getMessage());
